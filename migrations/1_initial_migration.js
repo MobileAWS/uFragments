f1d53964fe55c1773660a16530a450d4358b5370
lcjgarcia <jgarcia@littlecast.com>
2020-08-13 14:47:59 -0700

    updating initial migration

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,14 @@
-var Migrations = artifacts.require("./Migrations.sol");
+const Migrations = artifacts.require('./Migrations.sol');
+const _require = require('app-root-path').require;
+const truffleConfig = _require('/truffle.js');
+const accounts = truffleConfig.accounts;
 
-module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+module.exports = function (deployer, network) {
+  const deployerAccount = accounts[0];
+  const config = truffleConfig.networks[network];
+  const deploymentConfig = {
+    gas: config.gas,
+    from: deployerAccount
+  };
+  deployer.deploy(Migrations, deploymentConfig);
 };
